eb470ca0e014a6ef9702bd463cd357a09f08c572
Matias Miguez <matiaspakua@gmail.com>
2021-08-25 15:34:53 -0300

    change welcome message

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love docker!");
+    res.json("First complete example of docker and travis!");
 });
 
 app.listen(PORT, () => {
